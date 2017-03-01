e47919dccfeabc2b61bda3ffc9a4b412603503df
Peter Krevenets <peter.k.nets@gmail.com>
2017-03-01 15:56:17 +0200

    Add some basic styling

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import CalculatorDisplay from './calculator_display';
 import CalculatorKeypad from './calculator_keypad';
+import '../../style/style.css';
 
 export default class App extends Component {
   render() {
--- src/components/calculator_display.js
+++ src/components/calculator_display.js
@@ -4,7 +4,8 @@ export default class CalculatorDisplay extends Component {
   render() {
     return (
       <div className="calculator-display">
-        Display
+        <div className="calculator-display-input">Input</div>
+        <div className="calculator-display-result">Result</div>
       </div>
     );
   }
