1cd650bfce239ab392edb446fc4c6071db7fed9b
Peter Krevenets <peter.k.nets@gmail.com>
2017-03-01 15:02:15 +0200

    Add classNames to main components

--- src/components/calculator_display.js
+++ src/components/calculator_display.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class CalculatorDisplay extends Component {
   render() {
     return (
-      <div>
+      <div className="calculator-display">
         Display
       </div>
     );
--- src/components/calculator_keypad.js
+++ src/components/calculator_keypad.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class CalculatorKeypad extends Component {
   render() {
     return (
-      <div>
+      <div className='calculator-keypad'>
         Keypad
       </div>
     );
