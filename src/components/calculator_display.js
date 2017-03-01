5764711e8fa076fa5528ef9ada0754d44d8509eb
Peter Krevenets <peter.k.nets@gmail.com>
2017-03-01 17:11:21 +0200

    Add improved styling for calculator display in mobile view

--- src/components/calculator_display.js
+++ src/components/calculator_display.js
@@ -4,7 +4,9 @@ export default class CalculatorDisplay extends Component {
   render() {
     return (
       <div className="calculator-display">
-        <div className="calculator-display-input">Input</div>
+        <div className="calculator-display-input">
+          Input
+        </div>
         <div className="calculator-display-result">Result</div>
       </div>
     );
