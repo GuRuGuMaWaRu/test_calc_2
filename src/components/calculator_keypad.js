5f2335d9b74d98729e8347392943c0b8842cdaf5
Peter Krevenets <peter.k.nets@gmail.com>
2017-03-02 14:08:44 +0200

    Style display and keyboard

--- src/components/calculator_keypad.js
+++ src/components/calculator_keypad.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 
 export default class CalculatorKeypad extends Component {
   render() {
+    const keys = ['C', '()', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '.', '0', '+/-', '='],
+          keypad = keys.map(key => <div key={key} className='calculator-keypad-keypad-key'>{key}</div>);
     return (
       <div className='calculator-keypad'>
         <div className='calculator-keypad-pan'></div>
-        <div className='calculator-keypad-keypad'>Keypad</div>
+        <div className='calculator-keypad-keypad'>{keypad}</div>
         <div className='calculator-keypad-pan'></div>
       </div>
     );
