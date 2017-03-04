8f3d863fce27d0c83c65d7cdc0652dccd41560e0
Peter Krevenets <peter.k.nets@gmail.com>
2017-03-04 11:08:58 +0200

    Refactor layout & CSS

--- src/components/calculator_display.js
+++ src/components/calculator_display.js
@@ -3,13 +3,17 @@ import React, { Component } from 'react';
 export default class CalculatorDisplay extends Component {
   render() {
     return (
+      // <div className="calculator-display">
+      //   <div className="calculator-display-pan"></div>
+      //   <div className="calculator-display-display">
+      //     <div className="calculator-display-input">Input</div>
+      //     <div className="calculator-display-result">Result</div>
+      //   </div>
+      //   <div className="calculator-display-pan"></div>
+      // </div>
       <div className="calculator-display">
-        <div className="calculator-display-pan"></div>
-        <div className="calculator-display-display">
-          <div className="calculator-display-input">Input</div>
-          <div className="calculator-display-result">Result</div>
-        </div>
-        <div className="calculator-display-pan"></div>
+        <div className="calculator-display-input">Input</div>
+        <div className="calculator-display-result">Result</div>
       </div>
     );
   }
--- src/components/calculator_keypad.js
+++ src/components/calculator_keypad.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 export default class CalculatorKeypad extends Component {
   render() {
     const keys = ['C', '()', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '.', '0', '+/-', '='],
-          keypad = keys.map(key => <div key={key} className='calculator-keypad-keypad-key'>{key}</div>);
+          keypad = keys.map(key => <div key={key} className='calculator-keypad-key'>{ key }</div>);
     return (
+      // <div className='calculator-keypad'>
+      //   <div className='calculator-keypad-pan'></div>
+      //   <div className='calculator-keypad-keypad'>{keypad}</div>
+      //   <div className='calculator-keypad-pan'></div>
+      // </div>
       <div className='calculator-keypad'>
-        <div className='calculator-keypad-pan'></div>
-        <div className='calculator-keypad-keypad'>{keypad}</div>
-        <div className='calculator-keypad-pan'></div>
+        { keypad }
       </div>
     );
   }
