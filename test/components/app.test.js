74ebb255dea6c4695afd75eb60fc62a1dd197733
Peter Krevenets <peter.k.nets@gmail.com>
2017-03-04 13:49:53 +0200

    Add more simple tests for components

--- test/components/app.test.js
+++ test/components/app.test.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from '../../src/components/app';
+import CalculatorDisplay from '../../src/components/calculator_display';
+import CalculatorKeypad from '../../src/components/calculator_display';
 
 describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+  
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
-  it('shows a calculator display', () => {
-
+  it('shows display', () => {
+    const calculatorDisplay = <CalculatorDisplay />;
+    expect(wrapper.contains(calculatorDisplay)).toEqual(true);
   });
-  it('shows a calculator keypad', () => {
-
+  it('shows keypad', () => {
+    const calculatorKeypad = <CalculatorKeypad />;
+    expect(wrapper.contains(calculatorKeypad)).toEqual(true);
   });
 });
--- test/components/calculator_display.test.js
+++ test/components/calculator_display.test.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import CalculatorDisplay from '../../src/components/calculator_display';
 
 describe('CalculatorDisplay', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CalculatorDisplay />);
+  });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<CalculatorDisplay />, div);
+  });
+  it('shows input field', () => {
+    const inputField = <div className="calculator-display-input">Input</div>;
+    expect(wrapper.contains(inputField)).toEqual(true);
+  });
+  it('shows result field', () => {
+    const resultField = <div className="calculator-display-result">Result</div>;
+    expect(wrapper.contains(resultField)).toEqual(true);
   });
 });
