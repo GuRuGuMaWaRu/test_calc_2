6eb1790735f4becdcb25226ccf7e887d39ea1cc5
Peter Krevenets <peter.k.nets@gmail.com>
2017-03-04 11:34:46 +0200

    Clean up unused HTML

--- src/components/calculator_display.js
+++ src/components/calculator_display.js
@@ -3,14 +3,6 @@ import React, { Component } from 'react';
 export default class CalculatorDisplay extends Component {
   render() {
     return (
-      // <div className="calculator-display">
-      //   <div className="calculator-display-pan"></div>
-      //   <div className="calculator-display-display">
-      //     <div className="calculator-display-input">Input</div>
-      //     <div className="calculator-display-result">Result</div>
-      //   </div>
-      //   <div className="calculator-display-pan"></div>
-      // </div>
       <div className="calculator-display">
         <div className="calculator-display-input">Input</div>
         <div className="calculator-display-result">Result</div>
--- src/components/calculator_keypad.js
+++ src/components/calculator_keypad.js
@@ -5,11 +5,6 @@ export default class CalculatorKeypad extends Component {
     const keys = ['C', '()', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '.', '0', '+/-', '='],
           keypad = keys.map(key => <div key={key} className='calculator-keypad-key'>{ key }</div>);
     return (
-      // <div className='calculator-keypad'>
-      //   <div className='calculator-keypad-pan'></div>
-      //   <div className='calculator-keypad-keypad'>{keypad}</div>
-      //   <div className='calculator-keypad-pan'></div>
-      // </div>
       <div className='calculator-keypad'>
         { keypad }
       </div>
