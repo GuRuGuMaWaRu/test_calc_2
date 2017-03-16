16a5a189dab1737e643f99c817813d652198abf1
Peter Krevenets <peter.k.nets@gmail.com>
2017-03-16 10:06:40 +0200

    Update a failing Input reducer test

--- test/reducers/input.test.js
+++ test/reducers/input.test.js
@@ -6,7 +6,7 @@ describe('Input Reducer', () => {
     expect(inputReducer(undefined, {})).toEqual({});
   });
   it('handles action of type SET_INPUT', () => {
-    const action = {type: SET_INPUT, payload: '12334'};
-    expect(inputReducer({}, action)).toEqual({unparsedInput: '12334'});
+    const action = {type: SET_INPUT, payload: { parsed: '12334', display: '12334', result: '12334' }};
+    expect(inputReducer({}, action)).toEqual({ parsed: '12334', display: '12334', result: '12334' });
   });
 });
